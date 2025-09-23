de8e5c9e9e6243ccc25d0fdd9b63d36a6f125add
NGUYENVODUCCR <nguyenvoduc2004@gmail.com>
2025-09-23 11:06:17 +0700

    init project with docker

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Trang login
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # Kiểm tra username, password đơn giản
+        if username == "admin" and password == "123":
+            return redirect(url_for("success"))
+        else:
+            return render_template("login.html", error="Sai tài khoản hoặc mật khẩu!")
+
+    return render_template("login.html")
+
+# Trang success
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
